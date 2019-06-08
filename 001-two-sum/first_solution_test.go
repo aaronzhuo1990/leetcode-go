d40f910c7fa2e728809d3294676af1900630c2ac
aaronzhuo1990 <aaronchuo1990@gmail.com>
2019-06-08 15:39:19 -0600

    001-two-sum: fix go fmt

--- 001-two-sum/first_solution_test.go
+++ 001-two-sum/first_solution_test.go
@@ -6,16 +6,16 @@ import (
 
 func TestTwoSum(t *testing.T) {
 	tests := [][]int{
-		[]int{3, 4, 5},
-		[]int{0, 8, 7, 3, 4},
+		{3, 4, 5},
+		{0, 8, 7, 3, 4},
 	}
 	targets := []int{
 		9,
 		11,
 	}
 	results := [][]int{
-		[]int{1, 2},
-		[]int{1, 2},
+		{1, 2},
+		{1, 2},
 	}
 	caseNum := 2
 	for i := 0; i < caseNum; i++ {
