e76a834223d01012e45ebb17e4b560eb863f7164
aaronzhuo1990 <aaronchuo1990@gmail.com>
2019-06-23 15:09:39 -0600

    082-remove-duplicates-from-sorted-list-ii: readme and the first solution

---
+++ 082-remove-duplicates-from-sorted-list-ii/first_solution.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"github.com/azhuox/leetcode-go/000datastruct"
+)
+
+func main() {
+
+	input := datastruct.MakeSingleLinkedList([]int{1, 1, 2, 2, 3, 4, 4, 5})
+	result := removeDuplicates(input)
+
+	fmt.Printf("result: ") // Should be 3 -> 5
+	for result != nil {
+		fmt.Printf("%d ", result.Val)
+		result = result.Next
+	}
+	fmt.Printf("\n")
+}
+
+func removeDuplicates(list *datastruct.ListNode) *datastruct.ListNode {
+
+	if list == nil || list.Next == nil {
+		return list
+	}
+
+	first := list
+	for first.Val == first.Next.Val {
+		first = first.Next
+	}
+	if first != list {
+		return removeDuplicates(first.Next)
+	}
+
+	newHead := first
+
+	second := first.Next
+	for second != nil && second.Next != nil {
+
+		if second.Val == second.Next.Val {
+			for second.Next != nil && second.Val == second.Next.Val {
+				second = second.Next
+				continue
+			}
+			// Remove duplicates
+			second = second.Next
+		}
+
+		first.Next = second
+		first = second
+		second = second.Next
+	}
+
+	return newHead
+}
