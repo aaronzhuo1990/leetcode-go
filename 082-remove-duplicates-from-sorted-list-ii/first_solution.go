07e1351be21ee073ce78d907491506049f02125b
aaronzhuo1990 <aaronchuo1990@gmail.com>
2019-06-25 14:04:52 -0600

    082-remove-duplicates-from-sorted-list-ii: readme and the first solution

--- 082-remove-duplicates-from-sorted-list-ii/first_solution.go
+++ 082-remove-duplicates-from-sorted-list-ii/first_solution.go
@@ -56,12 +56,10 @@ func removeDuplicates(list *datastruct.ListNode) *datastruct.ListNode {
 
 // Recursion
 func recRemoveDuplicates(head *datastruct.ListNode) *datastruct.ListNode {
-	// 长度 <=1 的 list ，可以直接返回
 	if head == nil || head.Next == nil {
 		return head
 	}
 
-	// 要么 head 重复了，那就删除 head
 	if head.Val == head.Next.Val {
 		for head.Next != nil && head.Val == head.Next.Val {
 			head = head.Next
@@ -69,7 +67,6 @@ func recRemoveDuplicates(head *datastruct.ListNode) *datastruct.ListNode {
 		return recRemoveDuplicates(head.Next)
 	}
 
-	// 要么 head 不重复，递归处理 head 后面的节点
 	head.Next = recRemoveDuplicates(head.Next)
 	return head
 }
